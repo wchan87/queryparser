de3e4460803563e646af7b42be90eeb55f260eae
chanwr <wlechan87@gmail.com>
2020-07-22 22:06:08 -0400

    Cleaned up code to persist AST into neo4j

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 * https://docs.microsoft.com/en-us/sql/t-sql/queries/select-examples-transact-sql?view=sql-server-ver15
 * https://docs.gradle.org/current/userguide/antlr_plugin.html
 * https://github.com/antlr/grammars-v4/tree/master/sql/tsql
+* https://github.com/antlr/antlr4/blob/master/doc/case-insensitive-lexing.md#custom-character-streams-approach
--- src/main/java/org/chanwr/QueryParser.java
+++ src/main/java/org/chanwr/QueryParser.java
@@ -1,5 +1,17 @@
 package org.chanwr;
 
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.CaseChangingCharStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.Trees;
+import org.chanwr.parser.TSqlLexer;
+import org.chanwr.parser.TSqlParser;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
@@ -9,6 +21,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.neo4j.driver.Values.parameters;
 
@@ -35,11 +51,54 @@ public class QueryParser implements AutoCloseable {
 
     public void parseSqls() {
         try (Session session = driver.session()) {
-            var result = session.run("MATCH (f:SqlFile) RETURN f.sql");
+            var result = session.run("MATCH (f:SqlFile) RETURN id(f), f.sql");
             result.forEachRemaining(o -> {
-                o.get("f.sql");
+                // https://github.com/antlr/antlr4/blob/master/doc/case-insensitive-lexing.md#custom-character-streams-approach
+                CharStream s = CharStreams.fromString(o.get("f.sql").asString());
+                CaseChangingCharStream upper = new CaseChangingCharStream(s, true);
+                Lexer lexer = new TSqlLexer(upper);
+                TokenStream tokens = new CommonTokenStream(lexer);
+                TSqlParser parser = new TSqlParser(tokens);
+                // tsql_file is the root of the grammar
+                ParseTree tree = parser.tsql_file();
+                // https://github.com/antlr/antlr4/blob/antlr4-master-4.8-1/runtime/Java/src/org/antlr/v4/runtime/tree/Trees.java#L39
+                List<String> ruleNames = Arrays.asList(parser.getRuleNames());
+                // TODO log error on else
+                if (parser.getNumberOfSyntaxErrors() == 0) {
+                    parseAndSaveToGraphDb(session, o.get("id(f)").asInt(), ruleNames, tree);
+                }
+                // TODO cleanup with MATCH (n) WHERE any (x in labels(n) WHERE x <> 'SqlFile') DETACH DELETE n
+
+                // https://stackoverflow.com/questions/23809005/how-to-display-antlr-tree-gui
+                // printStringTree(ruleNames, tree);
+                // openTreeViewer(ruleNames, tree);
             });
         }
+    }
+
+    private void parseAndSaveToGraphDb(Session session, int id, List<String> ruleNames, ParseTree tree) {
+        String text = Trees.getNodeText(tree, ruleNames);
+        if (tree instanceof RuleContext) {
+            // insert current node and return
+            int currentId = session.run("MATCH (n) WHERE id(n) = $id CREATE (m:tsql_" + text + ")-[:IS_CHILD_OF]->(n) RETURN id(m)", Map.of("id", id)).single().get("id(m)").asInt();
+            for (int i = 0; i < tree.getChildCount(); i++) {
+                parseAndSaveToGraphDb(session, currentId, ruleNames, tree.getChild(i));
+            }
+        } else {
+            int currentId = session.run("MATCH (n) WHERE id(n) = $id CREATE (m:tsql_VALUE)-[:IS_CHILD_OF]->(n) SET m.text = $text RETURN id(m)", Map.of("id", id, "text", text)).single().get("id(m)").asInt();
+            for (int i = 0; i < tree.getChildCount(); i++) {
+                parseAndSaveToGraphDb(session, currentId, ruleNames, tree.getChild(i));
+            }
+        }
+    }
+
+    private void printStringTree(List<String> ruleNames, ParseTree tree) {
+        System.out.println(Trees.toStringTree(tree, ruleNames));
+    }
+
+    private void openTreeViewer(List<String> ruleNames, ParseTree tree) {
+        TreeViewer viewer = new TreeViewer(ruleNames, tree);
+        viewer.open();
     }
 
     @Override
